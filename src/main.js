fd61aa2cbaee51c23cde852d82f1e6f64a818ac6
pumelotea <535553297@qq.com>
2019-01-04 16:43:11 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,47 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import store from './store/store'
+import NoticePlugin from './api/notice'
+Vue.use(NoticePlugin)
+import Bus from './api/bus'
+Vue.use(Bus)
+import API from './api'
+Vue.use(API)
+import Msg from './api/msg'
+Vue.use(Msg)
+import DB from './api/db'
+Vue.use(DB)
+import Vtalk from './api/vtalk'
+Vue.use(Vtalk)
+
+
+import CxltToastr from 'cxlt-vue2-toastr'
+import 'cxlt-vue2-toastr/dist/css/cxlt-vue2-toastr.css'
+let toastrConfigs = {
+  position: 'top right',
+  showDuration: 400,
+  hideDuration:2000,
+  timeOut:3000,
+  successColor:'#28d17c',
+  infoColor:'#28c0de',
+  warningColor:'#f57d1b',
+  errorColor:'#ff666b',
+  showMethod:'slideInRight'
+}
+Vue.use(CxltToastr, toastrConfigs)
+
 
 Vue.config.productionTip = false
 
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
+
+
