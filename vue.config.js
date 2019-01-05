49049188ee4ed8edda71f619bd07d77ba83649a4
pumelotea <535553297@qq.com>
2019-01-05 16:24:00 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -13,6 +13,15 @@ module.exports = {
             }
         }
     },
-
-    lintOnSave: false
+    lintOnSave: false,
+    css: {
+        // 是否使用css分离插件 ExtractTextPlugin
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: true,
+        // css预设器配置项
+        loaderOptions: {},
+        // 启用 CSS modules for all css / pre-processor files.
+        modules: false
+    },
 }
